d63b98262b764b6113ee16b231b548805f44e752
Josh <n10526552@qut.edu.au>
2023-12-03 16:14:36 +1000

    started day3

--- Day3/Day3.ts
+++ Day3/Day3.ts
@@ -1,15 +1,15 @@
 import * as fs from "fs";
 
+const numbers: number[] = [];
 fs.readFile("./Day3/testStrings.txt", "utf-8", (err, data) => {
   if (err) {
     console.error("Error reading file: ", err);
     return;
   }
   const textFileInput = data.split("\n");
-  for (let line of textFileInput) {
+  for (const line of textFileInput) {
   }
 });
 
 let test1 = ["word", "banana"];
-
 console.log(test1[0][1]);
