88fffddd63c23d5643135d6adcdd9e947ce35c0c
Josh <n10526552@qut.edu.au>
2023-12-03 20:13:36 +1000

    got numbers for each row

--- Day3/Day3.ts
+++ Day3/Day3.ts
@@ -1,13 +1,21 @@
 import * as fs from "fs";
 
-const numbers: number[] = [];
 fs.readFile("./Day3/testStrings.txt", "utf-8", (err, data) => {
   if (err) {
     console.error("Error reading file: ", err);
     return;
   }
   const textFileInput = data.split("\n");
+  let lineNum = 0;
   for (const line of textFileInput) {
+    const lineItems = line.split(/\./g);
+    const lineNumbers = lineItems
+      .filter((item) => item.trim() !== "")
+      .filter((item) => !isNaN(Number(item)));
+    if (lineNumbers.length > 0) {
+      console.log(lineNumbers);
+    }
+    lineNum++;
   }
 });
 
