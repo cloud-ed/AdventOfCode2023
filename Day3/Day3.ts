c38acaacc4a314586e3f3d4a89f1d0dbadb7c73d
Josh <n10526552@qut.edu.au>
2023-12-03 22:14:38 +1000

    bulk of code done, debugging time

--- Day3/Day3.ts
+++ Day3/Day3.ts
@@ -1,23 +1,84 @@
 import * as fs from "fs";
 
+const symbols = [
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "-",
+  "_",
+  "+",
+  "=",
+  "\\",
+  "/",
+];
+
 fs.readFile("./Day3/testStrings.txt", "utf-8", (err, data) => {
   if (err) {
     console.error("Error reading file: ", err);
     return;
   }
   const textFileInput = data.split("\n");
-  let lineNum = 0;
+
+  // Start zero-based line number counting
+  let currentLineNumber = 0;
+  let sumPartNumber = 0;
   for (const line of textFileInput) {
+    // Split the string by '.', meaning you only get numbers and characters
     const lineItems = line.split(/\./g);
     const lineNumbers = lineItems
+
+      // Remove blank strings in array and any that are not numbers
       .filter((item) => item.trim() !== "")
       .filter((item) => !isNaN(Number(item)));
-    if (lineNumbers.length > 0) {
-      console.log(lineNumbers);
-    }
-    lineNum++;
-  }
-});
 
-let test1 = ["word", "banana"];
-console.log(test1[0][1]);
+    // If the array is empty then ignore it
+    if (lineNumbers.length > 0) {
+      // For each set of numbers in the line
+      for (const numSet of lineNumbers) {
+        const currentNumIndex = line.indexOf(numSet);
+
+        // Search through all surrounding positions of the number set to see if they are adjacent to a symbol, if so return setIsValid as true
+        let setIsValid = false;
+        const surroundingCharacters: string[] = [];
+        // Goes on the layer above, at and below the number set
+        for (let y = -1; y <= 1; y++) {
+          // Goes to the layer before, on and after the number set
+          for (let x = -1; x <= numSet.length + 1; x++) {
+            // Sets the xPos and yPos to the current search position
+            const yPos = currentLineNumber + y;
+            const xPos = currentNumIndex + x;
+
+            // Check if the position is within bounds
+            if (
+              yPos >= 0 &&
+              yPos < textFileInput.length &&
+              xPos >= 0 &&
+              xPos < textFileInput[currentLineNumber].length
+            ) {
+              surroundingCharacters.push(textFileInput[yPos][xPos]);
+            }
+          }
+        }
+        // If any of the surrounding characters
+        for (const character of surroundingCharacters) {
+          if (symbols.some((symbol) => character.includes(symbol))) {
+            setIsValid = true;
+            break;
+          }
+        }
+        if (setIsValid) {
+          sumPartNumber += Number(numSet);
+        }
+      }
+      currentLineNumber++;
+    }
+  }
+  console.log(sumPartNumber);
+});
